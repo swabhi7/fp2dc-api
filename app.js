e8f187ef716fa21eb1c6bfc4ab08bf9f8f42be21
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-08-18 23:04:11 +0530

    [feat] : Read port from env [chore] : Install dotenv

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const dotenv = require("dotenv");
+
+// Take variables from .env file and put them in process.env
+dotenv.config();
+
 app.get("/", (req, res) => {
   res.send("You have reached FP2DC API.");
 });
 
-app.listen(3333, console.log("API is running on port 3333"));
+const port = process.env.PORT || 3333;
+
+app.listen(port, console.log(`API is running on port ${port}`));
