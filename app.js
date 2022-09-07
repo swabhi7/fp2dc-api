edb6a29b45aa881a9acd77e9623c89579a769c74
Swabhimaan Dash <dashswabhimaan@gmail.com>
2022-09-07 23:04:57 +0530

    [feat] : Fetching recipes from DB and sending in response

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ const app = express();
 
 const dotenv = require("dotenv");
 const connectDatabase = require("./database/connectDatabase");
+const Recipe = require("./database/models/recipe.model");
 
 // Take variables from .env file and put them in process.env
 dotenv.config();
 
 connectDatabase();
 
-app.get("/", (req, res) => {
-  res.send("You have reached FP2DC API.");
+app.get("/", async (req, res) => {
+  const recipes = await Recipe.find({});
+  res.json(recipes);
 });
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, console.log(`API is running in ${process.env.NODE_ENV} environment on port ${port}`));
+app.listen(
+  port,
+  console.log(
+    `API is running in ${process.env.NODE_ENV} environment on port ${port}`
+  )
+);
